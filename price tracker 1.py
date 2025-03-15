9180dc4988994560e6e2b3161ac3dadc261ff4ef
Aish11186 <aishitiwari1@gmail.com>
2025-03-15 13:19:12 +0530

    Update price tracker 1.py

--- price tracker 1.py
+++ price tracker 1.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 
-URL= 'https://www.amazon.in/Fresh-Onion-1kg-Pack/dp/B07BG62MBV/ref=sr_1_1_f3_0o_fs?crid=1ITTDLKZPGJY&dib=eyJ2IjoiMSJ9.4eguaIy6zyytBGhlj2SQtF88nZ8GTGJeIW6ObBXoYVL_VnqtugBw0jt2EEaUqVJp2JEdQSC5XMw2PMhpnDuezMsVH5LD4Y57ChAfNWHND3eODjZxNizLsvqjLpzG70XcE5tfoCW7d5weJYbJkokiekT5z2ap-llzL3Hyzb5uDQ8wl8KLOKTaPOB8Ljtxw9e4B8JD7QgZXGqim2_ZCQWpc29Q3H7RtC5jRGtPYsJ7D7dgGcdA1gjtoQjZGTsYvZInGAtIhUQ-RLLJubH64UiShtj3xGiO9fMbnCN48cjCpHI.hvTdGbZSqkDgAw2B9sWiDU7yxNEZixhr7s8OmGByuTQ&dib_tag=se&keywords=onion&qid=1741878170&sprefix=onio%2Caps%2C351&sr=8-1&th=1'
+
+
+URL= input ("enter amazon link of the product you want to track")
 
 headers = { "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 OPR/117.0.0.0'}
 
@@ -26,14 +28,16 @@ def sendmail():
     server.ehlo()
     server.login('extra.ash1811@gmail.com','ajqp rhoa crow psqx')
     subject= "CHECK OUT THE PRICE!"
-    body= 'price just dropped check out the link https://www.amazon.in/Fresh-Onion-1kg-Pack/dp/B07BG62MBV/ref=sr_1_1_f3_0o_fs?crid=1ITTDLKZPGJY&dib=eyJ2IjoiMSJ9.4eguaIy6zyytBGhlj2SQtF88nZ8GTGJeIW6ObBXoYVL_VnqtugBw0jt2EEaUqVJp2JEdQSC5XMw2PMhpnDuezMsVH5LD4Y57ChAfNWHND3eODjZxNizLsvqjLpzG70XcE5tfoCW7d5weJYbJkokiekT5z2ap-llzL3Hyzb5uDQ8wl8KLOKTaPOB8Ljtxw9e4B8JD7QgZXGqim2_ZCQWpc29Q3H7RtC5jRGtPYsJ7D7dgGcdA1gjtoQjZGTsYvZInGAtIhUQ-RLLJubH64UiShtj3xGiO9fMbnCN48cjCpHI.hvTdGbZSqkDgAw2B9sWiDU7yxNEZixhr7s8OmGByuTQ&dib_tag=se&keywords=onion&qid=1741878170&sprefix=onio%2Caps%2C351&sr=8-1&th=1'
+    body= 'price just dropped check out the link www.amazon.in'
     msg= f"subject:{subject}\n\n{body}"
+
+    receiver1= input("enter mail")
 
 
 
     server.sendmail(
         'extra.ash1811@gmail.com',
-        'akshhatt1403@gmail.com',
+        reciever1,
         msg
 
     )
